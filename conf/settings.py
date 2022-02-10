3759169cd27a79e7ccec55e2fcd1edd7bae2fb76
Md Al Imran <tech.alimran@gmail.com>
2022-02-10 17:49:33 +0600

    project set up complated

---
+++ conf/settings.py
@@ -0,0 +1,136 @@
+from distutils.debug import DEBUG
+import os
+import environ
+import dj_database_url
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(DEBUG=(bool, False))
+env_file = os.path.join(BASE_DIR, ".env.keep")
+environ.Env.read_env(env_file)
+
+AUTH_USER_MODEL = 'user.User'
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = True
+# DEBUG = env('DEBUG', default=(bool, True))
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+
+INSTALLED_APPS = [
+    # core
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # third party
+    'rest_framework',
+
+    # project
+    'user',
+]
+
+# Applications only used when DEBUG = True
+
+# if DEBUG:
+#     INSTALLED_APPS += [
+#         'debug_toolbar',
+#         'django_extensions',
+#     ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Middlewares only used when DEBUG = True
+
+# if DEBUG:
+#     MIDDLEWARE += [
+#         'debug_toolbar.middleware.DebugToolbarMiddleware',
+#     ]
+
+ROOT_URLCONF = 'conf.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'conf.wsgi.application'
+
+DATABASES = {
+    'default': dj_database_url.config(default=env('DB_URL'))
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT', ]
+
+CORS_ALLOW_HEADERS = ['accept', 'accept-encoding', 'authorization', 'content-type',
+                      'dnt', 'origin', 'user-agent', 'x-csrftoken', 'x-requested-with', ]
+
+CSRF_TRUSTED_ORIGINS = []
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+STATIC_URL = '/static/'
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))), 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add additional constants only applicable for DEBUG = True
+
+if DEBUG:
+    INTERNAL_IPS = [
+        '127.0.0.1',
+        'localhost',
+    ]
---
+++ conf/urls.py
@@ -0,0 +1,13 @@
+import debug_toolbar
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('user.urls'))
+]
+
+# URLs only used when DEBUG = True
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
