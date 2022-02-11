40361a278dec24538cf8809bcd7ac6f85b7d357b
Md Al Imran <tech.alimran@gmail.com>
2022-02-11 14:36:11 +0600

    survey list added to admin panel

--- surveys/views.py
+++ surveys/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Survey
+
 
 # Create your views here.
+@login_required
+def survey_list(request):
+    """User can view all their surveys"""
+    surveys = Survey.objects.filter(creator=request.user).order_by("-created_at").all()
+    return render(request, "survey-list.html", {"surveys": surveys})
